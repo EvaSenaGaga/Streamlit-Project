185535116adff4fd81ccee54201c758e6d7188ba
Eva Sena Gaga <111463003+EvaSenaGaga@users.noreply.github.com>
2022-12-10 21:13:02 +0000

    Add files via upload

---
+++ Streamlit project/streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+
+with header:
+# Application title
+    st.title('STORE SALES PREDICTION')
+
+#App description
+    st.write('This app is made to predict store sales in Corporation favorita store use case.')
+
+with features:
